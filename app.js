f23a13b06dc6315284d88af10eb7a40cf3cf9140
Jordan Montero <jordanmontero@jordans-mbp.attlocal.net>
2021-02-28 11:26:20 -0800

    DONE

--- app.js
+++ app.js
@@ -6,17 +6,28 @@ const getStrainInfo = async (inputValue) => {
   try {
     removeStrainSearch()
     const response = await axios.get(url)
-    const grabData = response.data
-    grabData.forEach(grabData => {
-      displayStrainInfo(grabData) //This line fetches the name, species, and description. Also the ID that we use for the second api call
-      const button = document.querySelector(`#seeEffects${grabData.id}`) //Grab our medical button
-      button.addEventListener('click', (e) => {
+    const grabData = response.data ?? "No results found, please try again."
+    if (grabData.length >= 1) { //we set it to 1 because incorrect values give an array with a length of 0
+      grabData.forEach(grabData => {
+        displayStrainInfo(grabData) //This line fetches the name, species, and description. Also the ID that we use for the second api call
+        const button = document.querySelector(`#seeEffects${grabData.id}`) //Grab our medical button
+        button.addEventListener('click', (e) => {
           e.preventDefault()
           const effects = getEffects(grabData.id) //This is using the ID from first api call and adding it to this button (button#seeEffects${id})
           const medicalUl = document.querySelector(`#medical${grabData.id}`) //Grab our UL (medical list) we made in displayStrainInfo function
           medicalUl.insertHTML(effects) //Here we are inserting our effects variable, which runs the id through the get effects function, into that empty UL
         })
-    })
+      })
+    } else {
+      const resultsContainer = document.querySelector('.results-container')
+      const noResults = `
+        <div id = "noData">
+        <h1>No results found. Please try again.</h1>
+        </div>
+      `
+      resultsContainer.innerHTML = noResults
+
+    }
     return response
   } catch (err) {
     console.error(err)
@@ -57,22 +68,24 @@ window.addEventListener('load', (e) => {
 // Used this to prevent button from running more than once https://stackoverflow.com/questions/19053917/enable-the-button-to-be-clicked-only-to-once-exception/19053960
 // Append the strain data to the div ('.search-results')
 function displayStrainInfo(grabData) {
-  const strainList = document.querySelector('.results-container')
+  const resultsContainer = document.querySelector('.results-container')
   const strainInfo = `
   <div id = "strainStuff">
     <h1 class = "strainName">${grabData.name}</h1>
     <h3 class = "race">Species: ${grabData.race}</h3>    
-    <p class = "description"><strong>Description:</strong> ${grabData.desc}</p>
+    <p class = "description"><strong>Description:</strong> ${grabData.desc ?? "No description found"}</p>
     <button  id="seeEffects${grabData.id}" onClick="this.disabled = true;">Click here for medicinal use!</button>
     <ul id = "medical${grabData.id}"></ul>
   </div>
-  ` // Here we have to add grabData.id to the button and UL so that each button only goes with its specific ID.
-  strainList.insertAdjacentHTML("beforeend", strainInfo)
-}
+  `
 
+  // Here we have to add grabData.id to the button and UL so that each button only goes with its specific ID.
+  resultsContainer.insertAdjacentHTML("beforeend", strainInfo)
+}
+// https://www.youtube.com/watch?v=v2tJ3nzXh8I&t=41s line 65 nullish coalescing (es7)
 // Event Listener for "GO" Button
-const searchBtn = document.querySelector('#go')
-searchBtn.addEventListener('click', (e) => {
+const goBtn = document.querySelector('#go')
+goBtn.addEventListener('click', (e) => {
   e.preventDefault()
   const userInput = document.querySelector('input').value
   getStrainInfo(userInput)
@@ -90,44 +103,9 @@ userInput.addEventListener("keyup", (e) => {
 
 // Remove the strain search from html
 function removeStrainSearch() {
-  const strainContainer = document.querySelector('.results-container')
-  while (strainContainer.lastChild) {
-    strainContainer.removeChild(strainContainer.lastChild)
+  const resultsContainer = document.querySelector('.results-container')
+  while (resultsContainer.lastChild) {
+    resultsContainer.removeChild(resultsContainer.lastChild)
   }
 }
 
-// If/Else for description = null
-// function removeEmptyDescription() {
-//   const description = document.querySelector('.description') //from line 65 p we make
-//   const value = `${grabData.desc}`
-//   value.each(description, function (key, value)){
-//     if (value === "" || value === null) {
-//       delete description[key]
-//     } else {
-//       `${grabData.desc}`
-//     }
-//   }
-// }
-
-  // function removeEmptyDescription() {
-  //   const value = `${grabData.desc}`
-  //   if (value === null || value === "") {
-  //     const description = document.querySelector('.description')
-  //     description.insertHTML('No description available')
-  //   } else {
-  //     `${grabData.desc}`
-  //   }
-  // }
-
-
-// If/Else for no results found @displayStraininfo(grabData)
-
-// if (displayStrainInfo(grabData) === null) {
-//   console.log("No results found. Please try again.")
-    // let errorMessage = document.createElement('h1')
-    // errorMessage.textContent = "No results found. Please try again."
-    // let results = document.querySelector(".results-container")
-    // document.results.append(errorMessage)
-// } else {
-//   IF WE GOT THE DATA, SHOW THE DATA
-// }
